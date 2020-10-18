67adc1482baf86e91dcb38ef7ce0b42e18ab5aea
Fysho <jamesryan2232@gmail.com>
2020-10-18 14:13:57 +1100

    added imports

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import PingInterface
 def main():
     csvWriter = CsvWriter.CsvWriter("sonarData.csv")
     pingInterface = PingInterface.PingInterface(csvWriter)
-    myPingInterface.readArgs()
-    myPingInterface.connectToPing()
+    pingInterface.readArgs()
+    pingInterface.connectToPing()
 
-    
+
     newData = {}
     for i in range(20):
         newData[i] = [1,2,3] #save all data values corresponding to each grad step
